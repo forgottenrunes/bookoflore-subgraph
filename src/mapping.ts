1d1ef94bda9c26e266d6f6d575ae505d7ffeff0a
nik <nik@saberr.com>
2021-08-21 19:56:37 +0100

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,71 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  BookOfLore,
+  LoreAdded,
+  LoreStruck,
+  LoreUpdated,
+  NarrativeAdded,
+  NarrativeUpdated,
+  OwnershipTransferred
+} from "../generated/BookOfLore/BookOfLore"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleLoreAdded(event: LoreAdded): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.wizardId = event.params.wizardId
+  entity.loreIdx = event.params.loreIdx
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.loreAt(...)
+  // - contract.loreFor(...)
+  // - contract.narrative(...)
+  // - contract.narrativeAt(...)
+  // - contract.numLore(...)
+  // - contract.numNarrative(...)
+  // - contract.owner(...)
+  // - contract.wizardLore(...)
+  // - contract.wizardsContractAddress(...)
+}
+
+export function handleLoreStruck(event: LoreStruck): void {}
+
+export function handleLoreUpdated(event: LoreUpdated): void {}
+
+export function handleNarrativeAdded(event: NarrativeAdded): void {}
+
+export function handleNarrativeUpdated(event: NarrativeUpdated): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
