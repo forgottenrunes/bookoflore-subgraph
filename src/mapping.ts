84c73b7fbd031ad819080a8f3d1346717146afff
Forgotten <forgottenrunes@protonmail.com>
2021-09-16 11:04:49 -0500

    generalized graph to support multiple lore contracts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,77 +1,102 @@
-import {Address, BigInt} from "@graphprotocol/graph-ts";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
-    BookOfLore,
-    LoreAdded,
-    LoreStruck,
-    LoreUpdated,
+  BookOfLore,
+  LoreAdded,
+  LoreStruck,
+  LoreUpdated
 } from "../generated/BookOfLore/BookOfLore";
-import {Lore, Wizard} from "../generated/schema";
+import { Lore, LoreToken } from "../generated/schema";
 
-function getLoreKey(tokenContract: Address, tokenId: BigInt, loreIndex: BigInt):string {
-    let keyPrefix = 'unknown';
-
-    if (isWizardContract(tokenContract)) {
-        keyPrefix = 'wizards';
-    }
-
-    return keyPrefix + "-" + tokenId.toString().padStart(5, "0") + "-" + loreIndex.toString().padStart(4, "0");
+// Currently, Graph only lets us sort by one field, so we build this key for an
+// easy sort
+function getLoreKey(
+  tokenContract: Address,
+  tokenId: BigInt,
+  loreIndex: BigInt
+): string {
+  return (
+    tokenContract.toHexString() + "-" + tokenId.toString().padStart(10, "0") + "-" + loreIndex.toString().padStart(10, "0")
+  );
 }
 
-function isWizardContract(contractAddress:Address):boolean {
-    return contractAddress == Address.fromString('0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42');
+function getLoreTokenKey(tokenContract: Address, tokenId: BigInt): string {
+  return tokenContract.toHexString() + "-" + tokenId.toString().padStart(10, "0");
 }
 
 export function handleLoreAdded(event: LoreAdded): void {
-    let loreKey = getLoreKey(event.params.tokenContract, event.params.tokenId, event.params.loreIdx);
+  let params = event.params;
+  let tokenContract = params.tokenContract;
+  let tokenId = params.tokenId;
+  let loreIdx = params.loreIdx;
 
-    let lore = new Lore(loreKey);
+  let loreKey = getLoreKey(tokenContract, tokenId, loreIdx);
 
-    if (isWizardContract(event.params.tokenContract)) {
-        let wizard = new Wizard(event.params.tokenId.toString());
-        wizard.save();
-        lore.wizard = wizard.id;
-    }
+  let lore = new Lore(loreKey);
 
-    lore.tokenContract = event.params.tokenContract;
-    lore.tokenId = event.params.tokenId;
-    lore.index = event.params.loreIdx;
+  let loreToken = new LoreToken(getLoreTokenKey(tokenContract, tokenId));
+  loreToken.tokenContract = tokenContract;
+  loreToken.tokenId = tokenId;
+  loreToken.save();
+  lore.loreToken = loreToken.id;
 
-    let contract = BookOfLore.bind(event.address)
-    let loreFromContract = contract.loreAt(event.params.tokenContract, event.params.tokenId, event.params.loreIdx, event.params.loreIdx)[0];
+  lore.tokenContract = tokenContract;
+  lore.tokenId = tokenId;
+  lore.index = loreIdx;
 
-    lore.loreMetadataURI = loreFromContract.loreMetadataURI;
-    lore.creator = loreFromContract.creator;
-    lore.nsfw = loreFromContract.nsfw;
-    lore.parentLoreId = loreFromContract.parentLoreId;
-    lore.struck = loreFromContract.struck;
+  let contract = BookOfLore.bind(event.address);
+  let loreFromContract = contract.loreAt(
+    tokenContract,
+    tokenId,
+    loreIdx,
+    loreIdx
+  )[0];
 
-    lore.txHash = event.transaction.hash.toHex();
-    lore.createdAtTimestamp = event.block.timestamp;
-    lore.createdAtBlock = event.block.number;
+  lore.loreMetadataURI = loreFromContract.loreMetadataURI;
+  lore.creator = loreFromContract.creator;
+  lore.nsfw = loreFromContract.nsfw;
+  lore.parentLoreId = loreFromContract.parentLoreId;
+  lore.struck = loreFromContract.struck;
 
-    lore.save()
+  lore.txHash = event.transaction.hash.toHex();
+  lore.createdAtTimestamp = event.block.timestamp;
+  lore.createdAtBlock = event.block.number;
+
+  lore.save();
 }
 
 export function handleLoreUpdated(event: LoreUpdated): void {
-    let loreKey = getLoreKey(event.params.tokenContract, event.params.tokenId, event.params.loreIdx);
+  let loreKey = getLoreKey(
+    event.params.tokenContract,
+    event.params.tokenId,
+    event.params.loreIdx
+  );
 
-    let lore = new Lore(loreKey);
+  let lore = new Lore(loreKey);
 
-    let contract = BookOfLore.bind(event.address)
-    let loreFromContract = contract.loreAt(event.params.tokenContract, event.params.tokenId, event.params.loreIdx, event.params.loreIdx)[0];
+  let contract = BookOfLore.bind(event.address);
+  let loreFromContract = contract.loreAt(
+    event.params.tokenContract,
+    event.params.tokenId,
+    event.params.loreIdx,
+    event.params.loreIdx
+  )[0];
 
-    // Only metadata and nsfw are updateable
-    lore.loreMetadataURI = loreFromContract.loreMetadataURI;
-    lore.nsfw = loreFromContract.nsfw;
+  // Only metadata and nsfw are updateable
+  lore.loreMetadataURI = loreFromContract.loreMetadataURI;
+  lore.nsfw = loreFromContract.nsfw;
 
-    lore.save()
+  lore.save();
 }
 
 export function handleLoreStruck(event: LoreStruck): void {
-    let loreKey = getLoreKey(event.params.tokenContract, event.params.tokenId, event.params.loreIdx);
+  let loreKey = getLoreKey(
+    event.params.tokenContract,
+    event.params.tokenId,
+    event.params.loreIdx
+  );
 
-    let lore = new Lore(loreKey);
+  let lore = new Lore(loreKey);
 
-    lore.struck = true;
-    lore.save()
+  lore.struck = true;
+  lore.save();
 }
